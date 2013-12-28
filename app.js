e55589d782e378f3d4591fcf81f420bdd0a586d2
iambchan <chanbessie@gmail.com>
2013-12-27 18:37:49 -0800

    first commits

---
+++ app.js
@@ -0,0 +1,47 @@
+var express = require('express');
+
+var app = express();
+
+app.use(express.bodyParser());
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+  
+app.use('/public', express.static(__dirname + '/public'));
+
+
+app.get('/', function(req, res){
+  res.render('index',
+  { title : 'Home' }
+  )
+  //res.send('Template for writing a message here');
+});
+
+app.post('/message', function(req, res){ 
+  console.log(req.params); 
+  
+  var message = {
+      "content": req.body.content,
+      "recipient": req.body.recipient,
+      "send_date": req.body.send_date,
+      "create_date": req.body.create_date  
+    };
+    
+    res.json(message);
+});
+
+
+app.get('/:message_id', function(req, res){
+  // get message from server/database
+  var message_id = req.params.message_id;
+  res.send(message_id);  
+  
+  // load message content
+  // delete message on server
+});
+
+
+
+
+
+app.listen(3000);
+console.log('Listening on port 3000');
