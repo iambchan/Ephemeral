59af83778d21d78a9cab86cb0f316952bdd66531
Bessie Chan <chanbessie@gmail.com>
2014-10-06 21:45:22 -0700

    - factored out the database call in scheduler. Calls a method in the models.js file instead.

--- scheduler.js
+++ scheduler.js
@@ -4,22 +4,22 @@ var schedule = require("node-schedule");
 exports.init = function(ephemeraldb, models) {
     var rule = new schedule.RecurrenceRule();
 
+    function onSuccess(ephemerals) {
+        console.log("found " + ephemerals.length + " ephemerals");
+        ephemerals.forEach(function(ephemeral) {
+            //do not send a notification email if it was already sent.
+            if (ephemeral.email_sent) {
+                return;
+            }
+            email.sendMail(ephemeraldb, models, ephemeral);
+        });
+    }
+
+    function onFailure(err) {
+        console.log(err);
+    }
     var j = schedule.scheduleJob(rule, function() {
         console.log("running email scheduler");
-
-        models.Ephemeral.find({
-            send_date: {
-                $lte: new Date()
-            }
-        }, function(err, results) {
-            console.log("found " + results.length + " ephemerals");
-            results.forEach(function(ephemeral) {
-                //do not send a notification email if it was already sent.
-                if (ephemeral.email_sent) {
-                    return;
-                }
-                email.sendMail(ephemeraldb, models, ephemeral);
-            });
-        });
+        ephemeraldb.getEphemeralsDueForSending(models, onSuccess, onFailure);
     });
 };
