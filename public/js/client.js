e0c00fcf80ada31f5a67e3e3424cad2be1ea0570
Bessie Chan <chanbessie@gmail.com>
2014-10-07 23:59:30 -0700

    - Added convenience buttons "today" "A month" for send date

--- public/js/client.js
+++ public/js/client.js
@@ -2,10 +2,21 @@
 Date.prototype.toDateInputValue = (function() {
     var local = new Date(this);
     local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
-    return local.toJSON().slice(0,10);
+    return local.toJSON().slice(0, 10);
 });
 
-$(document).ready( function() {
+function setTodaysDate() {
+    $('#send_date').val(new Date().toDateInputValue());
+    return false;
+}
+
+function setAMonthDate() {
+    var myDate = new Date();
+    myDate.setDate(myDate.getDate() + 31);
+    $('#send_date').val(myDate.toDateInputValue());
+    return false;
+}
+
+$(document).ready(function() {
     $('#send_date').val(new Date().toDateInputValue());
 });
-
