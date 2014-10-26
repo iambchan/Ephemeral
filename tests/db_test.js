4350911a7700a7bbedc21b5c4048751d21a19def
Bessie Chan <chanbessie@gmail.com>
2014-10-26 11:25:36 -0700

    - updated format of ephemeral message - db_test.js - not sure what happened  but sublime probably changed all of the tabs to spacing

--- tests/db_test.js
+++ tests/db_test.js
@@ -4,16 +4,16 @@
 var mongoose = require('mongoose');
 //configure dependencies
 var MONGO_URL = process.env.MONGOLAB_URI ||
-process.env.MONGOHQ_URL ||
-'mongodb://localhost/test';
+    process.env.MONGOHQ_URL ||
+    'mongodb://localhost/test';
 
 var PORT = process.env.PORT || 3000;
 
 mongoose.connect(MONGO_URL);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function callback () {
-  // yay!
+db.once('open', function callback() {
+    // yay!
 });
 
 var echan = require('../models');
@@ -21,28 +21,28 @@ var models = echan.init(mongoose);
 
 // create ephemeral
 var message = {
-	"content": "test messsage",
-	"recipient": "chanbessie@gmail.com",
-	"send_date": new Date(),
-	"from_user": "test"
+    "content": "test messsage",
+    "recipient": "chanbessie@gmail.com",
+    "send_date": new Date(),
+    "from_user": "test"
 };
 
 var new_message = new models.Ephemeral(message);
 
-new_message.save(function (err, new_message) {
-	console.log("Saved new message");
- 	testNumDueEphemerals();		
-	//testGetNumSentEphemerals();
-	//echan.updateEmailSentFlag(models, new_message);
+new_message.save(function(err, new_message) {
+    console.log("Saved new message");
+    testNumDueEphemerals();
+    //testGetNumSentEphemerals();
+    //echan.updateEmailSentFlag(models, new_message);
 });
 
 function onSuccess(message) {
-  	console.log(message);
+    console.log(message);
 }
 
 function onFailure(err) {
-  	console.log("test failed");
-  	console.log(err);
+    console.log("test failed");
+    console.log(err);
 }
 
 //TODO:
@@ -50,32 +50,26 @@ function onFailure(err) {
 //2. verify email sent flag is true after email is sent
 
 function testGetNumUnsentEphemerals() {
-	echan.getNumUnsentEphemerals(models, function(numEphemerals){
-		if(numEphemerals === 1) {
-			console.log("number of ephemerals: " + numEphemerals);
-			console.log("testGetNumUnSentEphemerals pass");
-		}
-		else {
-			console.log("expected 1 unsent ephemeral but actual: " + numEphemerals);
-		}
-	});
+    echan.getNumUnsentEphemerals(models, function(numEphemerals) {
+        if (numEphemerals === 1) {
+            console.log("number of ephemerals: " + numEphemerals);
+            console.log("testGetNumUnSentEphemerals pass");
+        } else {
+            console.log("expected 1 unsent ephemeral but actual: " + numEphemerals);
+        }
+    });
 }
 
 // Tests here
 function testNumDueEphemerals() {
-	echan.getEphemeralsDueForSending(models, function(ephemerals){
-		if(ephemerals.length == 1) {
-			console.log(ephemerals.length);
-			console.log("test get number of due ephemerals pass");
-		}
-		else {
-			console.log("expected 1 due ephemeral but actual: " + ephemerals.length);
-		}	
-	}, function(err) {console.log(err);});
+    echan.getEphemeralsDueForSending(models, function(ephemerals) {
+        if (ephemerals.length == 1) {
+            console.log(ephemerals.length);
+            console.log("test get number of due ephemerals pass");
+        } else {
+            console.log("expected 1 due ephemeral but actual: " + ephemerals.length);
+        }
+    }, function(err) {
+        console.log(err);
+    });
 }
-
-
-
-
-
-
