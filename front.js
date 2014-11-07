9b2ed2af4bfda693c240168e479ad051d838606f
Ray H <rymndhng@gmail.com>
2014-11-06 23:33:19 -0800

    Fixing front server

--- front.js
+++ front.js
@@ -1,16 +1,22 @@
 // I'm a front server!
 
 var app = require('express')(),
-var httpProxy = require('http-proxy');
+    httpProxy = require('http-proxy');
 
-var proxy = new httpProxy.RoutingProxy();
+var proxy = new httpProxy.createProxyServer();
 
 // We will dispatch based on the host
 app.use(function(req,res) {
 	if(req.header('host') == 'ephemeral.dev') {
-		proxy.proxyRequest(req, res, {host: 'localhost', port: 3000})
+        console.log('Proxying request to localhost:3000');
+        
+        // Uncomment me to forward a request
+        // proxy.web(req, res, {target: 'localhost:3000'})
 	} else if (req.header('host') == 'ephemeral.test') {
-		proxy.proxyRequest(req, res, {host: 'localhost', port: 3001})
+        console.log('Proxying request to localhost:3001');
+        
+        // Uncomment me to forward a request
+        // proxy.web(req, res, {target: 'localhost:3001'})
 	} else {
 		res.send(404, 'Host not found!')
 	}
